99b5ef4eea7bdd3657089b4b0dea18122864feb7
diogoheck6 <diogoheck6@gmail.com>
2025-12-04 19:29:01 -0300

    arrow_07

---
+++ fundamentos/arrow_07.js
@@ -0,0 +1,23 @@
+// somar(3)(4)(5)
+
+// const somar = (a) => {
+// 	return (b) => {
+// 		return (c) => {
+// 			return a + b + c
+// 		}
+// 	}
+// }
+
+// const somar = (a) => {
+// 	return (b) => (c) => a + b + c
+// }
+
+const somar = (a) => (b) => (c) => a + b + c
+
+console.log(somar(13)(14)(15)) // 42
+
+const subrair = (a, b) => a - b
+const bomDia = () => 'Bom dia!'
+
+console.log(subrair(10, 5)) // 5
+console.log(bomDia()) // Bom dia!
